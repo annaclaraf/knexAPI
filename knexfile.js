92a649b861df1422c0920022d18d2c188420053c
annaclaraf <anisferraz@gmail.com>
2022-05-06 00:51:56 -0300

    database connection

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host : 'localhost',
+      port : 3306,
+      user : 'root',
+      password : '',
+      database : 'knexapi'
+    }
+  }
+};
