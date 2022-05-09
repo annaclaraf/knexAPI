9ecd689a1e73278d66d48a73d47ae012c4aa5c6b
annaclaraf <anisferraz@gmail.com>
2022-05-09 13:17:42 -0300

    post routes

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,17 @@ const express = require('express')
 const routes = express.Router()
 
 const UserController = require('./controllers/UserController')
+const PostController = require('./controllers/PostController')
 
 routes.get('/users', UserController.index)
 routes.post('/users', UserController.create)
 routes.put('/users/:id', UserController.update)
 routes.delete('/users/:id', UserController.delete)
+
+routes.get('/posts', PostController.index)
+routes.get('/posts/:id', PostController.show)
+routes.post('/posts', PostController.create)
+routes.put('/posts/:id', PostController.update)
+routes.delete('/posts/:id', PostController.delete)
 
 module.exports = routes
